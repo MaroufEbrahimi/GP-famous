b14d186267e2bb21742746764ca78cbb4d484c92
marouf <maroufibrahimi7264@gmail.com>
2022-08-25 21:07:01 -0700

    added activate color for header link

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+// hader link activate
+const link = document.querySelectorAll('.linkHeader');
+function activeLink() {
+   link.forEach((ele) => {
+      ele.classList.remove('active');
+   });
+   this.classList.add('active');
+}
+link.forEach((ele) => {
+   ele.addEventListener('click', activeLink);
+});
+// End of header link activate
