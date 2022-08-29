57cc01d274c03168117cd6b75274bb892a5e84a1
marouf <maroufibrahimi7264@gmail.com>
2022-08-28 20:57:33 -0700

    when you click every button from portfolio show images from every part

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+function myFun() {
+   alert('Waiting For Future')
+}
+
 
 // hader link activate
 const link = document.querySelectorAll('.linkHeader');
@@ -13,6 +17,62 @@ link.forEach((ele) => {
 });
 // End of header link activate
 
-function myFun() {
-   alert('Waiting For Future')
+// Portfolio
+var btn_all = document.getElementById('all_btn');
+var app_btn = document.getElementById('app_btn');
+var card_btn = document.getElementById('card_btn');
+var web_btn = document.getElementById('web_btn');
+var element = document.getElementsByClassName('box');
+
+// show all images
+function allImages() {
+   for (a in element) {
+      element[a].style.display = "grid";
+      btn_all.style.backgroundColor = '#ffc451';
+      app_btn.style.backgroundColor = 'transparent';
+      card_btn.style.backgroundColor = 'transparent';
+      web_btn.style.backgroundColor = 'transparent';
+   }
+}
+// show app images
+function appImages() {
+   for (a in element) {
+      if (element[a].className == 'box app') {
+         element[a].style.display = "grid";
+         app_btn.style.backgroundColor = '#ffc451';
+         btn_all.style.backgroundColor = 'transparent';
+         card_btn.style.backgroundColor = 'transparent';
+         web_btn.style.backgroundColor = 'transparent';
+      } else {
+         element[a].style.display = "none";
+      }
+   }
+}
+// show card images
+function cardImages() {
+   for (var a in element) {
+      if (element[a].className == 'box card') {
+         element[a].style.display = "grid";
+         card_btn.style.backgroundColor = '#ffc451';
+         app_btn.style.backgroundColor = 'transparent';
+         web_btn.style.backgroundColor = 'transparent';
+         btn_all.style.backgroundColor = 'transparent';
+      } else {
+         element[a].style.display = "none";
+      }
+   }
+}
+// show web images
+function webImages() {
+   for (a in element) {
+      if (element[a].className == 'box web') {
+         element[a].style.display = "grid";
+         web_btn.style.backgroundColor = '#ffc451';
+         app_btn.style.backgroundColor = 'transparent';
+         card_btn.style.backgroundColor = 'transparent';
+         btn_all.style.backgroundColor = 'transparent';
+      } else {
+         element[a].style.display = "none";
+      }
+   }
 }
